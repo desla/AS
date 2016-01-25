9984e73d8f6b63702a4e14f78dc77a07502680e0
Denis Zinchenko <denxc@yandex.ru>
2016-01-25 12:03:58 +0300

    Исправление разбора сообщения.

--- AudioServer/AudioServer/Alvasoft/AudioServer/Communication/CommandSession.cs
+++ AudioServer/AudioServer/Alvasoft/AudioServer/Communication/CommandSession.cs
@@ -2,9 +2,12 @@
 using System.Runtime.InteropServices;
 using Alvasoft.Tcp;
 using log4net;
+using System.Net;
+using System.Text;
 
 namespace Alvasoft.AudioServer.Communication
 {
+
     /// <summary>
     /// Сессия команд.
     /// </summary>
@@ -28,27 +31,59 @@ namespace Alvasoft.AudioServer.Communication
         /// <inheritdoc />
         public void OnReceivedFrom(ClientConnection aClientConnection, byte[] aBuffer, int aBufferSize)
         {
-            Logger.Info("Получена команда звукового сообщения");
+            try {
+                Logger.Info("Получена команда звукового сообщения");
+                Logger.Info("Разбор сообщения...");                
 
-            var message = new SoundCommand();
-            var size = Marshal.SizeOf(typeof (SoundCommand));
-            var pHandle = Marshal.AllocHGlobal(size);
-            Marshal.Copy(aBuffer, 0, pHandle, aBufferSize);
-            Marshal.PtrToStructure(pHandle, message);
-            Marshal.FreeHGlobal(pHandle);
+                var message = new SoundCommand();
+                var size = Marshal.SizeOf(typeof(SoundCommand));
+                Logger.Info("Размер структуры звукового сообщения: " + size);
+                var pHandle = Marshal.AllocHGlobal(size);
+                Marshal.Copy(aBuffer, 0, pHandle, aBufferSize);
+                Marshal.PtrToStructure(pHandle, message);
+                Marshal.FreeHGlobal(pHandle);
 
-            var channels = new int[message.channelCount];
-            for (var i = 0; i < message.channelCount; ++i) {
-                channels[i] = message.channels[i];
+                Logger.Info("Разбор сообщения завершен.");
+                message.priority = ReverceBits(message.priority);
+                Logger.Info("Приоритет: " + message.priority);
+                message.channelCount = ReverceBits(message.channelCount);
+                Logger.Info("Число каналов для объявления:" + message.channelCount);
+                message.fileCount = ReverceBits(message.fileCount);
+                Logger.Info("Число файлов для воспроизведения:" + message.fileCount);
+                var channelsStr = string.Empty;
+                for (var i = 0; i < message.channelCount && i < 15; ++i) {
+                    message.channels[i] = ReverceBits(message.channels[i]);
+                    channelsStr += message.channels[i] + " ";
+                }
+                Logger.Info("Список каналов для воспроизведения: " + channelsStr);
+
+                var filesStr = Encoding.ASCII.GetString(message.files);
+                var files = string.Empty;
+                for (var i = 0; i < message.fileCount && i < 5; ++i) {
+                    files += "\"" + filesStr.Substring(i * 20, 20).Trim('\0') + "\" ";
+                }
+                Logger.Info("Список файлов для воспроизведения: " + files);
+
+                if (message.channelCount > 0 && message.fileCount > 0 &&
+                    message.channelCount <= 15 && message.fileCount <= 5) {
+                    var channels = new int[message.channelCount];
+                    for (var i = 0; i < message.channelCount; ++i) {
+                        channels[i] = message.channels[i];
+                    }
+
+                    var fileNames = new string[message.fileCount];
+                    var messageFiles = Encoding.ASCII.GetString(message.files);
+                    for (var i = 0; i < message.fileCount; ++i) {
+                        fileNames[i] = messageFiles.Substring(i * 20, 20).Trim('\0');
+                    }
+
+                    if (callback != null) {
+                        callback.OnPlaySound(this, channels, message.priority, fileNames);
+                    }
+                }
             }
-
-            var fileNames = new string[message.fileCount];
-            for (var i = 0; i < message.fileCount; ++i) {
-                fileNames[i] = new string(message.files, i*20, 20).Trim('\0');                
-            }
-
-            if (callback != null) {
-                callback.OnPlaySound(this, channels, message.priority, fileNames);
+            catch (Exception ex) {
+                Logger.Error("Ошибка при обработке сообщения: " + ex.Message);
             }
         }
 
@@ -59,6 +94,16 @@ namespace Alvasoft.AudioServer.Communication
         public void SetCallback(CommandSessionCallback aCallback)
         {
             callback = aCallback;
+        }
+
+        /// <summary>
+        /// переворачивает биты в числе.
+        /// </summary>
+        /// <param name="aValue">Значение.</param>
+        /// <returns>Результат.</returns>
+        private int ReverceBits(int aValue)
+        {            
+            return IPAddress.NetworkToHostOrder(aValue);
         }
     }
 }
--- AudioServer/AudioServer/Alvasoft/AudioServer/Communication/SoundDataMessage.cs
+++ AudioServer/AudioServer/Alvasoft/AudioServer/Communication/SoundDataMessage.cs
@@ -2,18 +2,20 @@
 
 namespace Alvasoft.AudioServer.Communication
 {
+    using System;
+
     /// <summary>
     /// Структура, которая прилетает по сети от клиентов при команде на воспроизведения звука.
     /// </summary>
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public class SoundCommand
     {
-        public int channelCount;
+        public Int32 channelCount;
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 15)]
-        public int[] channels;
-        public int priority;
-        public int fileCount;
+        public Int32[] channels;
+        public Int32 priority;
+        public Int32 fileCount;
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5 * 20)]
-        public char[] files;
+        public byte[] files;
     }
 }
